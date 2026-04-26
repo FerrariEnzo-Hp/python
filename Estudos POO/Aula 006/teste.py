d7839caaeae16e7428800f31bff7468765574c3c
FerrariEnzo <enzorferrari06@gmail.com>
2026-04-26 14:20:11 -0300

    Update teste.py

--- Estudos POO/Aula 006/teste.py
+++ Estudos POO/Aula 006/teste.py
@@ -1,5 +1,9 @@
-pagina = 1
-avancar = 5
-while pagina != avancar:
-    pagina += 1
-    print(f"avançando página, página atual {pagina}")
+class Solution(object):
+    def pa(self, arr):
+        self.arr = arr
+        self.arr.sort()
+        if (self.arr[0] - self.arr[1]) == (self.arr[1] - self.arr[2]):
+            return True
+        else:
+            return False
+a1 = Solution()
